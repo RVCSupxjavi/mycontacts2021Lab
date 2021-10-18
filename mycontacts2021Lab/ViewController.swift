65101b6b5cf691b87ce127eca261a69db6b0ecc7
Javier Jimenez Jr <s0360579@student.rockvalleycollege.edu>
2021-10-18 18:08:30 -0500

    Initial Commit

---
+++ mycontacts2021Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mycontacts2021Lab
+//
+//  Created by Javier Jimenez Jr on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
